326ba0bcaf888ebc48dee0802d22642a9ab2af7d
Vitor Carvalho <vitorcarvalho.014@gmail.com>
2023-11-08 21:58:39 -0300

    Update teste.js

---
+++ teste.js
@@ -0,0 +1,31 @@
+var numeros = [1,2,3,4,5,6,7,8,9,10];
+
+/*
+for (let i = 0; i < numeros.length; i++) {
+    console.log("O valor de i eh:" + 1);
+    console.log(numeros[i]);
+}
+
+if(1 == 1) // Se for verdadeiro
+{
+    console.log(true);
+
+} else { // se não
+    console.log(false);
+}
+*/
+// Fazer um algoritmo que imprima os números do array que são maiores que 5;
+// Usando for e if else.
+
+
+for (let um = 1; um < numeros.length; um++) {
+   /* console.log (numeros[um]);*/
+let m5 = 5;
+if (m5 < numeros[um]) {
+    console.log(numeros[um]);
+} 
+}
+
+
+
+
