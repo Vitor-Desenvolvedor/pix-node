43b07568fc356c4a60e121b71fbbd572c5a7c941
Vitor Carvalho <vitorcarvalho.014@gmail.com>
2023-11-08 21:14:12 -0300

    Update

---
+++ desafio.js
@@ -0,0 +1,19 @@
+
+/**
+ * Crie um script que imprima o array: [1,2,3,1,3,4,5,6,4,5];
+ * Porém, antes de imprimir o array, substitua os valores duplicados por 0
+ */
+
+/*
+
+var Num_Duplicados = [1,2,3,1,3,4,5,6,4,5];
+
+Num_Duplicados[3]=0;
+Num_Duplicados[4]=0;
+Num_Duplicados[5]=0;
+Num_Duplicados[6]=0;
+
+console.log("Resultado:", Num_Duplicados);
+
+
+*/
